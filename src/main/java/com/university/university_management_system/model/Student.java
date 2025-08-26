c3def549361b02c8a86ba6a8a0e4ee2f62d9c79b
Alexandros Polymeros <alpolymer@gmail.com>
2025-08-26 15:21:40 +0300

    feat: created Student entity

---
+++ src/main/java/com/university/university_management_system/model/Student.java
@@ -0,0 +1,75 @@
+package com.university.university_management_system.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String firstname;
+
+    @Column (nullable = false)
+    private String lastname;
+
+    @Column (nullable = false)
+    private LocalDate enrollmentDate;
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstname, String lastname, LocalDate enrollmentDate) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", enrollmentDate='" + enrollmentDate + '\'' +
+                '}';
+    }
+}
